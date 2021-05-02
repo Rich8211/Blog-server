5ab433ed87e90483a8d1c831a00c763e55daa96b
Rich8211 <richlee821@gmail.com>
2021-05-02 18:43:13 -0400

    first commit

---
+++ models/postModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+
+    author: {type: String, required: true},
+    postImage: {type: String, required: true},
+    title: { type: String, required: true},
+    createdAt: {type: Date, default:Date.now},
+    tags: {type: [String]},
+    body: {type: String, required: true}
+    // html: {type: String, required: true}
+
+});
+
+module.exports = Post = mongoose.model("post", postSchema);
---
+++ routes/postRoutes.js
@@ -0,0 +1,71 @@
+const router  = require("express").Router();
+const Post = require("../models/postModel");
+const { uuid } = require('uuidv4');
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./public/");
+    },
+    filename: (req, file, cb) => {
+        const fileName = file.originalname.toLowerCase().split(" ").join("-");
+        cb(null, uuid() + "-" + fileName);
+    },
+});
+
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (
+            file.mimetype == "image/png" ||
+            file.mimetype == "image/jpg" ||
+            file.mimetype == "image/jpeg"
+        ) {
+            cb(null, true);
+        } else {
+            cb(null, false);
+            return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+        }
+    },
+});
+
+router.post("/", upload.single("postImage"),async (req, res) => {
+    const {title, createdAt, tags, body, author} = req.body;
+    const url = req.protocol + "://" + req.get("host");
+
+
+    
+    try{ 
+        if (req.isAuthenticated()) {
+            const newPost = new Post({
+            postImage: url + "/public/" + req.file.filename,
+            title,
+            createdAt,
+            tags,
+            body,
+            author
+        })
+        const savedPost = await newPost.save();
+        res.json(savedPost);
+        } else res.json("Not Authenticated")
+        
+    }
+    catch (err){
+        console.error(err)
+    }
+    
+});
+
+router.get("/", async (req,res) => {
+    const posts = await Post.find();
+    res.json(posts);
+});
+
+router.get("/:id", async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    res.json(post);
+});
+
+
+
+module.exports = router;
